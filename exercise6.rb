8859c7f7c7bb5ecd0fd42664027dc8da7caf89a7
Arya Iranpour <arya.iranpour@gmail.com>
2013-10-22 15:45:10 -0400

    Still on exercise6, going bananas

--- exercise6.rb
+++ exercise6.rb
@@ -1,13 +1,36 @@
-def printarray(array)
+def putsarray(array)
   array.each do |i|
     puts "* #{i}"
   end
 end
 
+def searcharray(array, what)
+  array.each do |i|
+    if i == what
+      return true
+   end
+  end
+end
+
 grocery_list = ["beets", "toilet paper", "apples", "chicken"]
 
-printarray(grocery_list)
-
+puts "My grocery list is:"
+putsarray(grocery_list)
+puts " "
 grocery_list << "rice"
+puts "Whoops, forgot to add rice, new list is:"
+putsarray(grocery_list)
+puts " "
 
-printarray(grocery_list)
+puts "Total number of items in my grocery list: #{grocery_list.length}"
+
+if searcharray(grocery_list, "bananas") == true
+  puts "You don't need to pick up bananas today."
+else
+  puts "You need to pick up bananas."
+end
+
+puts " "
+
+puts "The second item of the list is #{grocery_list[1]}."
+
