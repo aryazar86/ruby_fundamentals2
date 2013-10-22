d5aa237df68cb22ee0a9edb2fffdb00a8c3feb0d
Arya Iranpour <arya.iranpour@gmail.com>
2013-10-22 16:07:09 -0400

    Almost done exercise 7

---
+++ exercise7.rb
@@ -0,0 +1,32 @@
+def displayhash(thehash)
+  thehash.each do |x,y|
+    puts "#{x.to_s}: #{y.to_s} students"
+  end
+end
+
+def increaseamount(thehash)
+  thehash.each do |x,y|
+    thehash[x] = (y*1.05).round
+  end
+end
+
+students = {
+  :cohort1 => 34,
+  :cohort2 => 42,
+  :cohort3 => 22
+}
+displayhash(students)
+
+puts " "
+students[:cohort4] = 43
+puts "Added cohort4:"
+displayhash(students)
+
+puts " "
+puts "Cohort keys are:"
+puts "#{students.keys}"
+
+puts " "
+increaseamount(students)
+puts "Cohort increase by 5%:"
+displayhash(students)
